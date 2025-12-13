31c3bcc5d95100efb9092a72ae9e6bc058d75355
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-12 23:43:28 -0300

    Concatenando os valores em código

---
+++ 4- concatena.py
@@ -0,0 +1,20 @@
+# Alternativa do filme 1
+#Ultilizando o Input
+name = input("Digite o nome do filme:\n ")
+yearLaunch = int(input("Digite o ano de lançamento:\n"))
+noteMovie = float(input ("Digite a nota do filme:\n"))
+
+# # Alternativa do filme 2
+# print("Dados do filme: ")
+# print("==========================")
+# # print("Nome do filme: ",name)
+# # print("Ano de lançamento: ",yearLaunch)
+# # print("Digite a nota do filme: ",noteMovie)
+
+# print("Nome do filme:",name, "\nAno de lançamento:",yearLaunch,"\nDigite a nota do filme:",yearLaunch)
+
+# Alternativa do filme 3
+print(f"Digite o nome do filme:{name}\n"
+      f"Digite o ano de lançamento:{yearLaunch}\n"
+      f"Nota do filme:{noteMovie}\n"
+      )
