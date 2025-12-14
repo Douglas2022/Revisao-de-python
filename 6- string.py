93bbb372197692f6b6270afda174c65fc77a63e1
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-14 01:16:31 -0300

    Usando as strings

---
+++ 6- string.py
@@ -0,0 +1,21 @@
+movieName1 = "Top gun"
+movieName2 = "top gun"
+print(movieName1 == movieName2)
+
+movieDescription = """
+Top Gun Maverick
+é um filme de aviação
+e aventura
+muito consagrado
+na indútria
+
+"""
+print(movieName1)
+#Multiplicação das strings
+line = "="
+print(line * 50)
+# print("==================")
+print(movieDescription)
+#Procurar uma palavra no texto
+print("Top" in movieName1)
+print("ação" in movieName2)
