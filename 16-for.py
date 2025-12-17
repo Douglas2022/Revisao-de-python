7ff47f94fae5596dad68a4c6a876a240bfeb7ec5
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-17 20:50:27 -0300

    Quando o loop é encerrado!

--- 16-for.py
+++ 16-for.py
@@ -1,9 +1,14 @@
 #Lista de filmes
 movieList = ["Titanic","Godfather","Inception","Jurassic park"]
 # print(movieList)
-numeros = [10,20,30,40,50]
+# numeros = [10,20,30,40,50]
 #Internado valores de uma lista
 for movie in movieList:
     print(movie)
-for num in numeros:
-    print(num)
+# for num in numeros:
+#     print(num)
+#Quando a condição será atendida o loop será encerrado
+for movie in movieList:
+    if movie == "Inception":
+        break
+    print(movie)
