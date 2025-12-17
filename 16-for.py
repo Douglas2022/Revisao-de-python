fb6a1dc240aa942993e12af97d32803ffc7ce275
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-17 20:44:25 -0300

    Usando laços de repetição

---
+++ 16-for.py
@@ -0,0 +1,9 @@
+#Lista de filmes
+movieList = ["Titanic","Godfather","Inception","Jurassic park"]
+# print(movieList)
+numeros = [10,20,30,40,50]
+#Internado valores de uma lista
+for movie in movieList:
+    print(movie)
+for num in numeros:
+    print(num)
