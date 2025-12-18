9b37aba582a42557cb25fe9713357d44ec4c7554
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-17 21:18:31 -0300

    O loop for in ranger

--- 16-for.py
+++ 16-for.py
@@ -18,3 +18,16 @@ for movie in movieList:
     if movie == "Inception":
         continue
     print(movie)
+#Avaliação do filme.
+movieName = input("Digite o nome do filme:\n")
+movieRating = int(input("Quantas avaliações deseja fazer?\n"))
+total = 0
+for i in range(movieRating):
+    note = float(input("Digite a nota para o filme:\n"))
+    total += note
+if movieRating > 0:
+    averege = total / movieRating
+else:
+    averege = 0
+
+print(f"Média da avaliação do filme for{movieName} é: {averege:.2f}")
