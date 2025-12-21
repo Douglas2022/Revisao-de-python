3ca36adad6d9be1020eca1b8c3f2358783ed96b1
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-21 20:43:24 -0300

    Função para média de avaliações

--- 19-funcao.py
+++ 19-funcao.py
@@ -11,20 +11,20 @@
 #     welcome()
 
 #Função para calcular a média e as notas.
-def welcome():
-    print("Bem vindo ao sistema de filmes.")
-
-def calculate_averege():
-    num_ratings = int(input("Digite quantas avaliações deseja fazer para o filme:\n"))
+def calculete_avered():
+    num_ratings = int(input("Digite quantas avaliações deseja fazer pro filme:\n"))
     total = 0
+
     for i in range(num_ratings):
         note = float(input("Digite a nota para o filme:\n"))
         total += note
-    
+
     if num_ratings > 0:
         avered = total / num_ratings
+
     else:
         avered = 0
 
     return avered
-print(f"A média de avaliação é: {calculate_averege():.2f}")
+
+print(f"A média da avaliação é: {calculete_avered():.2f}")
