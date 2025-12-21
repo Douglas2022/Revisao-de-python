7f54c97ee58cb6fa0e3f1e249bcd5f75a2f466b1
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-20 23:07:39 -0300

    Lista comprehesion ultilizando o while

--- 18-list_comprehension.py
+++ 18-list_comprehension.py
@@ -13,3 +13,28 @@ print(movieShow)
 #Filmes que assistir
 movieWhatch = [movie for movie in movieList if movie != "Jurassic park"]
 print(movieWhatch)
+#Encontrando um filme pelo nome
+
+movieList = ["Titanic", "Godfather", "Inception", "Jurassic park"]
+
+while True:
+    seachName = input(
+        "Digite o nome do filme para buscar na lista (ou 'sair' para encerrar):\n"
+    )
+
+    if seachName.lower() == "sair":
+        print("Programa encerrado!")
+        break
+
+    foundMovies = [
+        movie for movie in movieList
+        if seachName.lower() in movie.lower()
+    ]
+
+    if foundMovies:
+        print(f"Filme(s) encontrado(s) com o nome '{seachName}':")
+        for movie in foundMovies:
+            print(movie)
+    else:
+        print(f"Nenhum filme foi encontrado com o nome '{seachName}'. Tente novamente!")
+
