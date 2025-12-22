7dcf5764a3985b7a1760e30c24798ce55e252695
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-21 21:35:29 -0300

    Função para somar

--- 20-args_func.py
+++ 20-args_func.py
@@ -3,3 +3,10 @@ def full_name(fastName,lastName):
     print(f"O nome é:{fastName} {lastName}")
 
 full_name("Fulano","Ciclano")
+full_name("Douglas","Nunes")
+full_name("Eliana","Nunes")
+
+#Função para somar dois números
+def sum_number(a,b):
+    return a + b
+print(f"Soma é:{sum_number(10,50)}")
