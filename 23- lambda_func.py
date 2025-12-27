a5be318c55a68518907a00c8d48a2bae346cd669
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-27 10:17:07 -0300

    Função que verifica se é par ou impa

---
+++ 23- lambda_func.py
@@ -0,0 +1,11 @@
+# Função potência de um número
+# power = lambda num : num **2
+# print(power(10))
+# print(power(9))
+# print(power(5))
+
+# Função se verifica se o número é par ou impa
+is_even = lambda x: x % 2 == 0;
+print(is_even(10))
+print(is_even(9))
+print(is_even(5))
